4a10e4f1a8afc88bf75b52ec1d09e5b2571147e9
Miquel Ruiz <self@miquelruiz.net>
2021-02-01 19:54:11 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="youtube-rss-subscriber",
-    version="0.0.1.dev1",
+    version="0.0.1",
     author="Miquel Ruiz",
     author_email="self@miquelruiz.net",
     description="Subscribe to YouTube channels without a YouTube account",
